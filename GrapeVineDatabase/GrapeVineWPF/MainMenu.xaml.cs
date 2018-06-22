3c6bb1ccae6afa246852aaf23332cc8f04ff5734
nicholasainsworth <101604238@student.swin.edu.au>
2018-06-22 15:24:55 +1000

    Attempt at completing Task 4 of Challenge

---
+++ GrapeVineDatabase/GrapeVineWPF/MainMenu.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GrapeVineAPI.Models;
+
+namespace GrapeVineWPF
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Page
+    {
+        HttpClient client = new HttpClient();
+        public MainMenu()
+        {
+            InitializeComponent();
+            List<TourEvents> tourEvents = new List<TourEvents>();
+
+            //resultsLV.Items.Add(new GameResults() { GameNo = "1", HomeTeam = "Boxhill Stags", HomeGoals = "0", AwayTeam = "Kew Knights", AwayGoals = "2" });
+        }
+
+        class TourEvents
+        {
+            public string TourName { get; set; }
+            public string EventDate { get; set; }
+            public string EventFee { get; set; }
+        }
+
+        private async void ViewData_Click(object sender, RoutedEventArgs e)
+        {
+            client.BaseAddress = new Uri("http://localhost:50622/api/");
+
+            HttpResponseMessage httpResponseMessage = await client.GetAsync("api/TourEvents");
+
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            var tourEvents = JsonConvert.DeserializeObject<List<TourEvent>>(content);
+
+            string json = JsonConvert.SerializeObject(tourEvents);
+
+            TourEventsLV.ItemsSource = json;
+
+            //TourEventsLV.Items.Add(new TourEvent() { tourName = "This actually worked", eventDate = Convert.ToDateTime("13/2/2016"), eventFee = Convert.ToDecimal(250.00) });
+
+        }
+    }
+}
---
+++ GrapeVineDatabase/GrapeVineWPF/Models/jsonplaceholder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapeVineWPF.Models
+{
+    class jsonplaceholder
+    {
+        class TourEvent
+        {
+            public string TourName { get; set; }
+            public string EventDate { get; set; }
+            public string EventFee { get; set; }
+        }
+
+        class TourEvents
+        {
+            public List<TourEvent> TourEventList { get; set; }
+        }
+    }
+}
